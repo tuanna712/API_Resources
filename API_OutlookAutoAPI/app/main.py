60df7b9fec6a590879cf9490ebf58d206e86db44
tuongnguso03 <40336908+tuongnguso03@users.noreply.github.com>
2024-06-07 16:29:43 +0700

    base64 test

--- API_OutlookAutoAPI/app/main.py
+++ API_OutlookAutoAPI/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File,UploadFile, requests
 from pydantic import BaseModel
+import base64
 from pdfplumber import open as open_pdf
 
 class ChatParams(BaseModel):
@@ -16,8 +17,10 @@ def hello_world():
 @app.post("/upload_pdf/")
 async def upload_pdf(file: UploadFile = File(...)):
     # Save the uploaded file
+    content = base64.b64decode(await file.read())
+    print("Content: ", content)
     with open(file.filename, "wb") as buffer:
-        buffer.write(await file.read())
+        buffer.write(content)
 
     # Read the contents of the PDF
     with open_pdf(file.filename) as pdf:
