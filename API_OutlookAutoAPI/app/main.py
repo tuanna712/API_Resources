e0155d8f99ba25145834010a30ef4baab7d6716e
tuongnguso03 <40336908+tuongnguso03@users.noreply.github.com>
2024-06-10 09:30:39 +0700

    update read xlsx

--- API_OutlookAutoAPI/app/main.py
+++ API_OutlookAutoAPI/app/main.py
@@ -1,7 +1,14 @@
+import os
+
 from fastapi import FastAPI, File,UploadFile, requests
 from pydantic import BaseModel
 import base64
 from pdfplumber import open as open_pdf
+import pandas as pd
+
+import aiosmtplib
+from email.message import EmailMessage
+
 
 class ChatParams(BaseModel):
     input: str
@@ -16,16 +23,35 @@ def hello_world():
         
 @app.post("/upload_pdf/")
 async def upload_pdf(file: UploadFile = File(...)):
-    # Save the uploaded file
-    content = base64.b64decode(await file.read())
-    print("Content: ", content)
-    with open(file.filename, "wb") as buffer:
-        buffer.write(content)
+    try:
+        ## Save the uploaded file
+        content = base64.b64decode(await file.read())
+        print("Content: ", content)
+        with open(file.filename, "wb") as buffer:
+            buffer.write(content)
 
-    # Read the contents of the PDF
-    with open_pdf(file.filename) as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
-    print(text)
-    return {"text": text}
+        if file.filename.endswith("pdf"):
+            # Read the contents of the PDF
+            with open_pdf(file.filename) as pdf:
+                text = ""
+                for page in pdf.pages[:min(3, len(pdf.pages))]:
+                    text += page.extract_text()
+
+        elif file.filename.endswith(("xlsx", "xls")):
+            dataframe = pd.read_excel(file.filename)
+            text = dataframe[:min(5, len(dataframe))].to_string()
+        
+        ## Start sending the mail
+        username = os.environ["username"]
+        password = os.environ["password"]
+        message = EmailMessage()
+        message["From"] = username
+        message["To"] = "metalwallcrusher@gmail.com"
+        message["Subject"] = f"File received from mail: {file.filename}"
+        message.set_content(f"File content: \n {text}")
+
+        await aiosmtplib.send(message, hostname="smtp-mail.outlook.com", port=587, username = username, password = password)
+        return {"result": "Done"}
+    except Exception as e:
+        return {"error": e}
+
